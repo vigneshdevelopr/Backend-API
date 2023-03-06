a7c0c687469fb672e136656a8cb948c5af0508fd
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-03-06 14:27:42 +0530

    undo previous commit

--- routes/students.js
+++ routes/students.js
@@ -38,9 +38,7 @@ router.get("/:id", async (req, res) => {
 });
 //post
 router.post("/", async (req, res) => {
-  const newdata = {name:req.body.name, batch: req.body.batch, age:req.body.age, qualification: req.body.qualification,
-  experience: req.body.experience
-  }
+  const newdata = req.body;
 
   try {
     const addData = await postStudent(newdata);
